3dffc375cdb4be9c769658d0505d5bf9aea6cb47
Stian Selnes <stian@pexip.com>
2015-10-23 10:58:20 +0200

    rtpbasepayload: rtpbasedepayload: Add source-info property
    Add a source-info property that will read/write meta to the buffers
    about RTP source information. The GstRTPSourceMeta can be used to
    transport information about the origin of a buffer, e.g. the sources
    that is included in a mixed audio buffer.
    
    A new function gst_rtp_base_payload_allocate_output_buffer() is added
    for payloaders to use to allocate the output RTP buffer with the correct
    number of CSRCs according to the meta and fill it.
    
    RTPSourceMeta does not make sense on RTP buffers since the information
    is in the RTP header. So the payloader will strip the meta from the
    output buffer.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=761947

--- gst-libs/gst/rtp/gstrtpbasepayload.h
+++ gst-libs/gst/rtp/gstrtpbasepayload.h
@@ -171,6 +171,18 @@ GST_EXPORT
 GstFlowReturn   gst_rtp_base_payload_push_list          (GstRTPBasePayload *payload,
                                                          GstBufferList *list);
 
+GstBuffer *     gst_rtp_base_payload_allocate_output_buffer (GstRTPBasePayload * payload,
+                                                             guint payload_len, guint8 pad_len,
+                                                             guint8 csrc_count);
+
+void            gst_rtp_base_payload_set_source_info_enabled (GstRTPBasePayload * payload,
+                                                              gboolean enable);
+
+gboolean        gst_rtp_base_payload_is_source_info_enabled (GstRTPBasePayload * payload);
+
+guint           gst_rtp_base_payload_get_source_count (GstRTPBasePayload * payload,
+                                                       GstBuffer * buffer);
+
 #ifdef G_DEFINE_AUTOPTR_CLEANUP_FUNC
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(GstRTPBasePayload, gst_object_unref)
 #endif
