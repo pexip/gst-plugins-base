b074e8901ac4720e316c1585dcf3a3ff7e90f5ee
Stian Selnes <stian@pexip.com>
2016-06-05 17:06:18 +0200

    videodecoder: Add API to drop frames before handle_frame on error [pexhack]
    A simple workaround for non-robust decoders (vp9).

--- gst-libs/gst/video/gstvideodecoder.h
+++ gst-libs/gst/video/gstvideodecoder.h
@@ -378,6 +378,10 @@ GST_EXPORT
 gint     gst_video_decoder_get_max_errors (GstVideoDecoder * dec);
 
 GST_EXPORT
+void     gst_video_decoder_set_drop_error_frames_early (GstVideoDecoder * dec,
+             gboolean early);
+
+GST_EXPORT
 void     gst_video_decoder_set_needs_format (GstVideoDecoder * dec,
                                              gboolean enabled);
 
